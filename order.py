f7082330834845994b2f8ae2d261a8e721dc3b2b
Ikrom Hotamov <ikrom.hotamov@gmail.com>
2016-02-11 21:43:39 +0100

    Order deliver method

--- order.py
+++ order.py
@@ -10,5 +10,18 @@ class Order(object):
         except KeyError:
             self.items[product] = 1
 
+    def deliver_product(self, product, product_quantity):
+        item_count = self.items[product]
+        if item_count < product_quantity:
+            raise Exception("Remaining to deliver %s products, but delivering too many: %s for order %s" % (item_count, product_quantity, self))
+        item_count -= product_quantity
+        if item_count == 0:
+            self.items.pop(product)
+        else:
+            self.items[product] = item_count
+
+    def completed(self):
+        return len(self.items.keys()) == 0
+
     def __repr__(self):
         return '<Order id=%r,location=%r>' % (self.id, self.location)

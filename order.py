a65c90882dfe687596739f4172a9732ea301723d
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2016-02-11 21:50:10 +0100

    order

--- order.py
+++ order.py
@@ -15,13 +15,15 @@ class Order(object):
         if item_count < product_quantity:
             raise Exception("Remaining to deliver %s products, but delivering too many: %s for order %s" % (item_count, product_quantity, self))
         item_count -= product_quantity
-        if item_count == 0:
-            self.items.pop(product)
-        else:
-            self.items[product] = item_count
+        if item_count < 0:
+            raise Exception("Item count cannot be less than zero %s" % item_count)
+        self.items[product] = item_count
 
     def completed(self):
-        return len(self.items.keys()) == 0
+        for _, value in self.items.iteritems():
+            if value > 0:
+                return False
+        return True
 
     def __repr__(self):
         return '<Order id=%r,location=%r>' % (self.id, self.location)
